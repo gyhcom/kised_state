8a8921f01240185fd1400cb8d455583f2115dbc6
leeheejae <leeheejae55@naver.com>
2024-12-02 16:29:19 +0900

    FEAT: 사용자 정보 수정 커밋 #21

---
+++ src/main/java/state/admin/userManage/application/processor/UserUpdateProcessor.java
@@ -0,0 +1,36 @@
+package state.admin.userManage.application.processor;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import state.admin.userManage.application.command.UserInfoUpdateCommand;
+import state.admin.userManage.domain.entity.User;
+import state.admin.userManage.domain.repository.UserRepository;
+
+@Component
+public class UserUpdateProcessor {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+
+    public UserUpdateProcessor(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void execute(UserInfoUpdateCommand userInfoUpdateCommand) {
+
+        if (!userRepository.existsById(userInfoUpdateCommand.getSeq())) {
+            throw new NullPointerException();
+        } else {
+            User user = userRepository.getReferenceById(userInfoUpdateCommand.getSeq());
+            //비밀번호 암호화
+            userInfoUpdateCommand.setPassword(passwordEncoder.encode(userInfoUpdateCommand.getPassword()));
+
+            user.setUserNm(userInfoUpdateCommand.getUserNm());
+            user.setEmail(userInfoUpdateCommand.getEmail());
+            user.setDeptCd(userInfoUpdateCommand.getDeptCd());
+            user.setManageCd(userInfoUpdateCommand.getManageCd());
+        }
+    }
+}
