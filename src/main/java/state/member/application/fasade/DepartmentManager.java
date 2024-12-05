7c1eb739c41b7eeee7c1ee1be2cef06383f82178
hgChoi <chlgusrms96@naver.com>
2024-12-05 17:33:49 +0900

    FIX: 병합 후 파일명으로 발생하는 이슈 수정

--- src/main/java/state/member/application/fasade/DepartmentManager.java
+++ src/main/java/state/member/application/fasade/DepartmentManager.java
@@ -2,13 +2,10 @@ package state.member.application.fasade;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import state.member.application.command.department.DepartmentInfoRequestCommand;
 import state.member.application.processor.department.DepartmentFindAllProcessor;
 import state.member.domain.entity.Department;
-import state.member.domain.repository.DepartmentRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Transactional
 @Service
