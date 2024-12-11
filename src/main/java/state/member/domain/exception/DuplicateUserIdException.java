b1feab74373c67edb0405e97922d5ae1b5a57ccb
hgChoi <chlgusrms96@naver.com>
2024-12-11 17:30:34 +0900

    FEAT: 1. 로그인 화면, dashboard 화면 security 추가 2. username->userId로 사용자 체크하도록 수정 3. logout 설정 추가

--- src/main/java/state/member/domain/exception/DuplicateUserIdException.java
+++
@@ -1,7 +0,0 @@
-package state.member.domain.exception;
-
-public class DuplicateUserIdException extends RuntimeException {
-    public DuplicateUserIdException() {
-        super(MemberErrorCode.DUPLICATE_USERID.getDescription());
-    }
-}
