3c2a3b8eb268a227f600da9b3f43de0dfbdfcf9d
hgChoi <chlgusrms96@naver.com>
2024-12-11 17:39:59 +0900

    FEAT: 1. 로그인 화면 구현 2. 대시보드 화면 구현, 3. 부트스트랩 탬플릿 사용

--- src/main/java/state/member/presentation/apis/DashBoardApi.java
+++ src/main/java/state/member/presentation/apis/DashBoardApi.java
@@ -1,14 +1,12 @@
 package state.member.presentation.apis;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Slf4j
 @Controller
-public class LoginApi {
-    @GetMapping("/loginForm")
-    public String loginForm() {
-        return "login";
+public class DashBoardApi {
+    @GetMapping("/dashboard")
+    public String dashboard() {
+        return "dashboard";
     }
 }
