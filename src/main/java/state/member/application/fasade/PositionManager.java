073ec2fd24d3bc387ce185c03bbc34f98ef02053
hgChoi <chlgusrms96@naver.com>
2024-12-16 14:03:47 +0900

    FEAT: 부서, 직위 조회 기능 추가

--- src/main/java/state/member/application/fasade/PositionManager.java
+++ src/main/java/state/member/application/fasade/PositionManager.java
@@ -2,19 +2,27 @@ package state.member.application.fasade;
 
 import org.springframework.stereotype.Service;
 import state.member.application.processor.position.PositionFindAllProcessor;
+import state.member.application.processor.position.PositionFindByIdProcessor;
 import state.member.domain.entity.Position;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PositionManager {
     private final PositionFindAllProcessor positionFindAllProcessor;
+    private final PositionFindByIdProcessor positionFindByIdProcessor;
 
-    public PositionManager(PositionFindAllProcessor positionFindAllProcessor) {
+    public PositionManager(
+            PositionFindAllProcessor positionFindAllProcessor,
+            PositionFindByIdProcessor positionFindByIdProcessor) {
         this.positionFindAllProcessor = positionFindAllProcessor;
+        this.positionFindByIdProcessor = positionFindByIdProcessor;
     }
 
     public List<Position> findAll() {
         return positionFindAllProcessor.execute();
     }
+
+    public Position findById(String id) {return positionFindByIdProcessor.execute(id);}
 }
