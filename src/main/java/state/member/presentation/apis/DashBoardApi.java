361b28f6f4529d58a9bd78569d29335ce4ca7a64
hgChoi <chlgusrms96@naver.com>
2025-01-07 14:38:04 +0900

    FEAT: 권한별 메뉴 조회 수정

--- src/main/java/state/member/presentation/apis/DashBoardApi.java
+++ src/main/java/state/member/presentation/apis/DashBoardApi.java
@@ -1,23 +1,40 @@
 package state.member.presentation.apis;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-import state.member.application.fasade.DashboardManager;
-import state.member.presentation.request.TempRequestDto;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Collection;
 
 @Slf4j
 @Controller
 public class DashBoardApi {
     @GetMapping("/dashboard")
-    public String dashboard() {
+    public String dashboard(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+
+        // 현재 인증 정보 가져오기
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        // 권한 정보 가져오기
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+
+        // 관리자 여부 확인
+        boolean isAdmin = authorities.stream()
+                .anyMatch(auth -> auth.getAuthority().equals("ROLE_ADMIN") || auth.getAuthority().equals("ROLE_SUPER"));
+
+        // 관리자일 경우 'auth' 값을 세션에 설정
+        if (isAdmin) {
+            session.setAttribute("auth", "Y");
+        } else {
+            session.setAttribute("auth", "N");
+        }
+
         return "dashboard";
     }
 }
